897e7602aef76075b59dbe425ef67dcd9488850f
Arthur Fernandes <arthur0139@gmail.com>
2024-12-30 22:50:07 -0300

    update

--- project_monday/views.py
+++ project_monday/views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 app_blueprint = Blueprint('index', __name__, template_folder='templates')
 
 
 @app_blueprint.route('/')
 def index():
-    return 'hello world'
+    return render_template('index.html')
