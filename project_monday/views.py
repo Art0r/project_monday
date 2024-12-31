b4e01c4478b8326ccd5a7d175efbb9d8fba4081f
Arthur Fernandes <arthur0139@gmail.com>
2024-12-31 01:28:49 -0300

    update

--- project_monday/views.py
+++ project_monday/views.py
@@ -6,3 +6,11 @@ app_blueprint = Blueprint('index', __name__, template_folder='templates')
 @app_blueprint.route('/')
 def index():
     return render_template('index.html')
+
+
+@app_blueprint.route('/random')
+def index_random():
+
+    import random
+
+    return render_template('random.html', r=int(random.random() * 100))
